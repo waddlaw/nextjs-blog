790894c604c067d96d53ea8b36834a78a59b700e
waddlaw <ingronze@gmail.com>
2021-01-30 04:09:45 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,7 +28,9 @@ class MyDocument extends Document {
               />
             </>
           )}
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Fraunces:wght@700&display=swap"/>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@700&display=swap
+"/>
         </Head>
         <Head />
         <body>
