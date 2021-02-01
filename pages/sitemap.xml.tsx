ba54e6627298f5fbdc45d2c5f67c1440776eced6
waddlaw <ingronze@gmail.com>
2021-02-01 18:20:49 +0900

    update

---
+++ pages/sitemap.xml.tsx
@@ -0,0 +1,45 @@
+import { GetServerSidePropsContext } from 'next';
+import { getAllPostIds } from '../lib/posts'
+
+export const getServerSideProps = async ({ res }: GetServerSidePropsContext) => {
+  const xml = await generateSitemapXml();
+
+  res.statusCode = 200;
+  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // 24時間のキャッシュ
+  res.setHeader('Content-Type', 'text/xml');
+  res.end(xml);
+
+  return {
+    props: {},
+  };
+};
+
+async function generateSitemapXml():Promise<string> {
+  const appHost = "https://blog.wado.dev/";
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+
+  // site root
+  xml += `
+      <url>
+        <loc>${appHost}</loc>
+      </url>
+    `
+  
+  // ここでurlを足していく
+  const posts = getAllPostIds();
+  posts.forEach(({ params: { id } }) => {
+    xml += `
+      <url>
+        <loc>${appHost}posts/${id}</loc>
+        <lastmod>${id}</lastmod>
+      </url>
+    `
+  })
+  
+  xml += `</urlset>`;
+  return xml;
+}
+
+const Page = () => null;
+export default Page;
